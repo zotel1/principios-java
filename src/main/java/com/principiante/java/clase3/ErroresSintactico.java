a80391968c7e90dade4b8f5f89bac73506aacd57
zotel1 <zotelsigel@gmail.com>
2025-05-15 23:46:04 -0300

    Errores sintacticos, mas conocidos cuando nos falt cerrar una llave o un punto y coma

---
+++ src/main/java/com/principiante/java/clase3/ErroresSintactico.java
@@ -0,0 +1,8 @@
+package com.principiante.java.clase3;
+
+// Un error sintactico es cuando falt el punto y coma
+public class ErroresSintactico {
+    public static void main(String args[]) {
+        System.out.println("Hola Errores");
+    }
+}
