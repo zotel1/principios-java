ac53bb2258255d0dd6b4a9c8dc8d18bdec2946b6
zotel1 <zotelsigel@gmail.com>
2025-05-15 23:36:40 -0300

    primer commit

---
+++ src/main/java/com/principiante/java/clase2/Cristian.java
@@ -0,0 +1,8 @@
+package com.principiante.java.clase2;
+
+public class Cristian {
+  // El metodo main es el que le da arranque a nuestro programa
+  public static void main(String args[]) {
+    System.out.println("Hola mundo");
+  }
+}
