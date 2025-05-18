f01235af796027263a70cb7034fff2b10d5ff58c
zotel1 <zotelsigel@gmail.com>
2025-05-18 02:45:10 -0300

    En esta clases vemos las variables y tipo de datos

---
+++ src/main/java/com/principiante/java/clase4/VariablesYTiposDeDatos.java
@@ -0,0 +1,12 @@
+package com.principiante.java.clase4;
+
+public class VariablesYTiposDeDatos {
+    public static void main(String args[]) {
+        int numero_uno = 5;
+        int numero_dos = 2;
+        int resultado = 0;
+
+        resultado = numero_uno + numero_dos;
+        System.out.println("El valor de tu suma es: " + resultado);
+    }
+}
