45c16ff874157085a01c8745ce1401f53edfff15
zotel1 <zotelsigel@gmail.com>
2025-05-17 23:51:33 -0300

    Operadores aritmeticos, primer ejemplo

---
+++ src/main/java/com/principiante/java/clase5/OperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+package com.principiante.java.clase5;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String args[]) {
+        int numero_uno = 4;
+        int numero_dos = 4;
+        int resultado = 0;
+
+        resultado = numero_uno + numero_dos / 2;
+        System.out.println("El resultado es: " + resultado);
+    }
+}
