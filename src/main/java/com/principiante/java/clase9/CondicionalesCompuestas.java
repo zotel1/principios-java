a4ca882054373e076bd2ca2118fddeb80654927b
zotel1 <zotelsigel@gmail>
2025-06-29 23:56:33 -0300

    update

--- src/main/java/com/principiante/java/clase9/CondicionalesCompuestas.java
+++ src/main/java/com/principiante/java/clase9/CondicionalesCompuestas.java
@@ -1,4 +1,17 @@
 package com.principiante.java.clase9;
 
 public class CondicionalesCompuestas {
-}
+
+    public static void main(String[] args){
+
+        String nombre = "";
+
+        int clave = 0;
+
+        int antiguedad = 0;
+
+        System.out.println("Bienvenido al sistema vacacional de Coca.Cola Company");
+
+    }
+
+    }
