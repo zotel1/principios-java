e02d2759a4ef6036f451f213de1c8cd8ef0159c4
zotel1 <zotelsigel@gmail.com>
2025-06-30 23:56:12 -0300

    Sistema de control vacacional

--- src/main/java/com/principiante/java/clase10/SistemaControlVacacional.java
+++ src/main/java/com/principiante/java/clase10/SistemaControlVacacional.java
@@ -1,4 +1,33 @@
 package com.principiante.java.clase10;
 
+import java.util.Scanner;
+
 public class SistemaControlVacacional {
+
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+
+        String nombre = "";
+
+        int clave = 0, antiguedad = 0;
+
+        System.out.println("*********************************************");
+        System.out.println("*Bienvenido al sistema vacacional de Coca-Cola Company*");
+        System.out.println("*********************************************");
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("¿Cual es el nombre del trabajador?: ");
+        nombre = entrada.nextLine();
+        System.out.println("");
+
+        System.out.println("¿Cuanto tiempo de servicio tiene el trabajador?: ");
+        antiguedad = entrada.nextInt();
+        System.out.println("");
+
+        System.out.println("¿Cuel es la clave del trabajador?: ");
+        clave = entrada.nextInt();
+        System.out.println("");
+
+    }
 }
