222b4ef47753f51c601449f6b5f1f4ff88307c19
zotel1 <zotelsigel@gmail.com>
2025-07-10 22:14:47 -0300

    En esta clase utilizamos el ciclo for

---
+++ src/main/java/com/principiante/java/clase12/CicloFor.java
@@ -0,0 +1,11 @@
+package com.principiante.java.clase12;
+
+public class CicloFor {
+
+    public static void main(String args[]) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + ", ");
+        }
+    }
+}
