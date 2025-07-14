f3d1d6a80bfd38df49a613fd85c0a3c7b13e2d30
zotel1 <zotelsigel@gmail.com>
2025-07-13 23:51:26 -0300

    En esta clase vemos el ciclo while

---
+++ src/main/java/com/principiante/java/clase13/CicloWhile.java
@@ -0,0 +1,13 @@
+package com.principiante.java.clase13;
+
+public class CicloWhile {
+    public static void main(String args[]){
+
+        int i = 1;
+
+        while (i < 10) {
+            System.out.println(i + ", el valor actual de nuestro entero");
+            i = i+1;
+        }
+    }
+}
