87578ea9f95422f58e5fa9a09c5acb7aa26aa058
zotel1 <zotelsigel@gmail.com>
2025-07-15 23:53:19 -0300

    tambien utilizamos la serie o ciclo con do while

--- src/main/java/com/principiante/java/clase15/ForWhileDoWhile.java
+++ src/main/java/com/principiante/java/clase15/ForWhileDoWhile.java
@@ -1,14 +1,14 @@
 package com.principiante.java.clase15;
 
 public class ForWhileDoWhile {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         int i = 0;
 
         System.out.println("Serie con for: ");
 
-        for(i = 1; i <= 10; i++){
-            if( i < 10) {
+        for (i = 1; i <= 10; i++) {
+            if (i < 10) {
                 System.out.print(i + ", ");
             } else {
                 System.out.print(i);
@@ -17,7 +17,7 @@ public class ForWhileDoWhile {
         i = 1;
         System.out.println("");
         System.out.println("Serie con while: ");
-        while (i <=10) {
+        while (i <= 10) {
             if (i < 10) {
                 System.out.print(i + ", ");
             } else {
@@ -25,5 +25,16 @@ public class ForWhileDoWhile {
             }
             i++;
         }
+        i = 1;
+        System.out.println("");
+        System.out.println("Serie con Do-While");
+        do {
+            if (i < 10) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        } while (i <= 10);
     }
 }
