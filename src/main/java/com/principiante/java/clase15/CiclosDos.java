03d092b91d998b096963dc653edd56bda93cd3f9
zotel1 <zotelsigel@gmail.com>
2025-07-16 00:09:13 -0300

    ademas usamos el ciclo while y el ciclo do while

--- src/main/java/com/principiante/java/clase15/CiclosDos.java
+++ src/main/java/com/principiante/java/clase15/CiclosDos.java
@@ -1,19 +1,50 @@
 package com.principiante.java.clase15;
 
 public class CiclosDos {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         int i = 1, j = 99;
         System.out.println("Serie con for: ");
-        for(i = 1; i <= 5; i++){
-            if(i < 5){
+        for (i = 1; i <= 5; i++) {
+            if (i < 5) {
                 System.out.print(i + ", ");
                 System.out.print(j + ", ");
             } else {
                 System.out.print(i + ", ");
-                System.out.println(j);
+                System.out.print(j);
             }
             j--;
         }
+        System.out.println("");
+
+        i = 1;
+        j = 99;
+        System.out.println("Serie con while: ");
+        while (i <= 5) {
+            if (i < 5) {
+                System.out.print(i + ", ");
+                System.out.print(j + ", ");
+            } else {
+                System.out.print(i + ", ");
+                System.out.print(j);
+            }
+            i++;
+            j--;
+        }
+        System.out.println("");
+        i = 1;
+        j = 99;
+        System.out.println("Serie con Do-While: ");
+        do {
+            if (i < 5) {
+                System.out.print(i + ", ");
+                System.out.print(j + ", ");
+            } else {
+                System.out.print(i + ", ");
+                System.out.print(j);
+            }
+            i++;
+            j--;
+        } while (i <= 5);
     }
 }
