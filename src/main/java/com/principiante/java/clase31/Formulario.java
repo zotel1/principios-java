0db757dd0448c426cee43d8ae298dd08e72403a3
zotel1 <zotelsigel@gmail.com>
2025-07-29 20:05:13 -0300

    En esta clase vemos el componente JScrollPane el cual nos permite agregarle un scroll a nuestro JTextArea

---
+++ src/main/java/com/principiante/java/clase31/Formulario.java
@@ -0,0 +1,32 @@
+package com.principiante.java.clase31;
+
+import javax.swing.*;
+
+public class Formulario extends JFrame {
+
+    private JTextField textField1;
+    private JScrollPane scrollPane1;
+
+    private JTextArea textArea1;
+
+    public Formulario(){
+        setLayout(null);
+        textField1 = new JTextField();
+        textField1.setBounds(10, 10, 200, 30);
+        add(textField1);
+
+        textArea1 = new JTextArea();
+        scrollPane1 = new JScrollPane(textArea1);
+        scrollPane1.setBounds(10, 50, 400, 300);
+        add(scrollPane1);
+    }
+
+    public static void main(String[] args) {
+        Formulario formulario1 = new Formulario();
+        formulario1.setVisible(true);
+        formulario1.setBounds(0, 0, 540, 400);
+        formulario1.setResizable(false);
+        formulario1.setLocationRelativeTo(null);
+
+    }
+}
