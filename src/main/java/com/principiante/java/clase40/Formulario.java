a42ad001267772589289b40f96ab8e17b2145cc3
zotel1 <zotelsigel@gmail.com>
2025-08-08 21:41:53 -0300

    En la clase 40, utilizamos el metodo JRadioButton para seleccionar de a un elemento, similar a un checkbok, pero a diferencia del mismo solo seleccionamos de a uno

---
+++ src/main/java/com/principiante/java/clase40/Formulario.java
@@ -0,0 +1,57 @@
+package com.principiante.java.clase40;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Formulario extends JFrame implements ChangeListener {
+
+    private JRadioButton radio1, radio2, radio3;
+    private ButtonGroup bg;
+
+    public Formulario(){
+        setLayout(null);
+
+        bg = new ButtonGroup();
+
+        radio1 = new JRadioButton("640*480");
+        radio1.setBounds(10,20,100,30);
+        radio1.addChangeListener(this);
+        add(radio1);
+        bg.add(radio1);
+
+        radio2 = new JRadioButton("800*600");
+        radio2.setBounds(10,70,100,30);
+        radio2.addChangeListener(this);
+        add(radio2);
+        bg.add(radio2);
+
+        radio3 = new JRadioButton("1024*768");
+        radio3.setBounds(10,120,100,30);
+        radio3.addChangeListener(this);
+        add(radio3);
+        bg.add(radio3);
+
+
+    }
+
+    public void stateChanged(ChangeEvent e){
+        if (radio1.isSelected()){
+            setSize(640, 480);
+        }
+        if(radio2.isSelected()){
+            setSize(800, 600);
+        }
+        if (radio3.isSelected()){
+            setSize(1024, 768);
+        }
+    }
+
+    public static void main(String[] args) {
+        Formulario formulario40 = new Formulario();
+        formulario40.setBounds(0,0, 350, 230);
+        formulario40.setVisible(true);
+        formulario40.setResizable(false);
+        formulario40.setLocationRelativeTo(null);
+    }
+}
