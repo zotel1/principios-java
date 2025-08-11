6142b955304e5ab40d54b3b31a5f80d55bb60df5
zotel1 <zotelsigel@gmail.com>
2025-08-11 00:40:31 -0300

    Terminamos deprepara la interface de nuestro proyecto

--- src/main/java/com/principiante/java/proyecto/Bienvenida.java
+++ src/main/java/com/principiante/java/proyecto/Bienvenida.java
@@ -2,7 +2,7 @@ package com.principiante.java.proyecto;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Bienvenida extends JFrame implements ActionListener {
 
@@ -14,6 +14,58 @@ public class Bienvenida extends JFrame implements ActionListener {
         setLayout(null);
         setTitle("Bienvenido");
         getContentPane().setBackground(new Color(255, 0, 0));
+        setIconImage(new ImageIcon(getClass().getResource("images/icon.png")).getImage());
 
+        ImageIcon imagen = new ImageIcon("images/logo-coca.png");
+        label1 = new JLabel(imagen);
+        label1.setBounds(25, 15, 300, 150);
+        add(label1);
+
+        label2 = new JLabel("Sistema de Control Vacacional");
+        label2.setBounds(35, 135, 300, 30);
+        label2.setFont(new Font("Andale Mono", 3, 18));
+        label2.setForeground(new Color(255,255,255));
+        add(label2);
+
+        label3 = new JLabel("Ingrese su nombre");
+        label3.setBounds(45, 212, 200, 30);
+        label3.setFont(new Font("Andale Mono", 1, 12));
+        label3.setForeground(new Color(255,255,255));
+        add(label3);
+
+        label4 = new JLabel("©2025 The Coca-Cola Company");
+        label4.setBounds(85, 375, 300, 30);
+        label4.setFont(new Font("Andale Mono", 1, 12));
+        label4.setForeground(new Color(255,255,255));
+        add(label4);
+
+        textField1 = new JTextField();
+        textField1.setBounds(45,240, 255, 25);
+        textField1.setBackground(new Color(224,224, 224));
+        label4.setFont(new Font("Andale Mono", 1, 14));
+        label4.setForeground(new Color(255,0,0));
+        add(textField1);
+
+        boton1 = new JButton();
+        boton1.setBounds(125,280, 100, 30);
+        boton1.setBackground(new Color(255,255, 255));
+        boton1.setFont(new Font("Andale Mono", 1, 14));
+        boton1.setForeground(new Color(255,0,0));
+        boton1.addActionListener(this);
+        add(boton1);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == boton1){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Bienvenida bienvenida = new Bienvenida();
+        bienvenida.setBounds(0, 0, 300, 400);
+        bienvenida.setVisible(true);
+        bienvenida.setResizable(false);
+        bienvenida.setLocationRelativeTo(null);
     }
 }
