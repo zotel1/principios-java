2d7e17d6f608f3259b50a85c44f867eb43c46fea
zotel1 <zotelsigel@gmail.com>
2025-08-10 23:58:41 -0300

    Agregamos ademas las imagenes que vamos a utilizar en el proyecto final

--- src/main/java/com/principiante/java/proyecto/Bienvenida.java
+++ src/main/java/com/principiante/java/proyecto/Bienvenida.java
@@ -1,6 +1,7 @@
 package com.principiante.java.proyecto;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Bienvenida extends JFrame implements ActionListener {
@@ -8,4 +9,11 @@ public class Bienvenida extends JFrame implements ActionListener {
     private JTextField textField1;
     private JLabel label1, label2, label3, label4;
     private JButton boton1;
+
+    public Bienvenida(){
+        setLayout(null);
+        setTitle("Bienvenido");
+        getContentPane().setBackground(new Color(255, 0, 0));
+
+    }
 }
