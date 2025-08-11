d39219c8e55a6da9de5caac3a42d0ad1f0023022
zotel1 <zotelsigel@gmail.com>
2025-08-10 23:55:12 -0300

    Comenzamos con el proyecto final, un formulario de bienvenida

---
+++ src/main/java/com/principiante/java/proyecto/Bienvenida.java
@@ -0,0 +1,11 @@
+package com.principiante.java.proyecto;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class Bienvenida extends JFrame implements ActionListener {
+
+    private JTextField textField1;
+    private JLabel label1, label2, label3, label4;
+    private JButton boton1;
+}
