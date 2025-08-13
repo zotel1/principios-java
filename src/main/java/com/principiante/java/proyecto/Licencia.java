e8cb9721c89310305e8dce6fabf734e65130971e
zotel1 <zotelsigel@gmail.com>
2025-08-13 01:14:30 -0300

    Agregamos la interface de la licencia

--- src/main/java/com/principiante/java/proyecto/Licencia.java
+++ src/main/java/com/principiante/java/proyecto/Licencia.java
@@ -1,8 +1,10 @@
 package com.principiante.java.proyecto;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Licencia extends JFrame implements ChangeListener, ActionListener {
@@ -28,5 +30,42 @@ public class Licencia extends JFrame implements ChangeListener, ActionListener {
         scrollPane1 = new JScrollPane(textArea1);
         scrollPane1.setBounds(10, 40, 575, 200);
         add(scrollPane1);
+
+        check1 = new JCheckBox("Yo Acepto");
+        check1.setBounds(10, 250, 300, 30);
+        check1.addChangeListener(this);
+        add(boton1);
+
+        boton1 = new JButton("Continuar");
+        boton1.setBounds(10, 290, 100, 30);
+        boton1.addActionListener(this);
+        boton1.setEnabled(false);
+        add(boton1);
+
+        boton2 = new JButton("No Acepto");
+        boton2.setBounds(120, 290,100, 30);
+        boton2.addActionListener(this);
+        add(boton2);
+
+        ImageIcon imagen = new ImageIcon("images/coca-cola.png");
+        label2 = new JLabel(imagen);
+        label2.setBounds(315, 135, 300,300);
+        add(label2);
+    }
+
+    public void stateChanged(ChangeEvent e){
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public static void main(String[] args) {
+        Licencia licencia = new Licencia();
+        licencia.setBounds(0,0, 600, 400);
+        licencia.setVisible(true);
+        licencia.setResizable(false);
+        licencia.setLocationRelativeTo(null);
     }
 }
